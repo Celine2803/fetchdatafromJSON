5d5b4f102820e93422ab90234f686597092c85bf
Celine Kerubo <celinekerubo28@gmail.com>
2023-02-13 19:28:41 +0300

    fetch data from json to ReactJS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+import Records from'./records.json'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* DISPLAYING JSON DATA IN JAVASCRIPT */}
+     ...webStylePress...
+     <br/><br/>
+     {
+      Records && Records.map(record =>{
+        return(
+          <div className="box" key={record.id}>
+           <strong> {record.title}</strong><br/>
+           {record.content}<br/>
+
+          { record.tech && record.tech.map(data=>{
+            return(
+              <div key={record.id}>
+                ..{data.name}..
+                </div>
+            )
+          })} 
+            </div>
+        )
+      })
+     }
     </div>
   );
 }
